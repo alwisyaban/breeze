e365f2a5f7841c757a402d6271852be0d71b909e
alwisyaban <alwisyaban96@gmail.com>
2024-12-31 14:50:19 +0700

    add edit password + see the password

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -8,30 +8,72 @@
 
                 <div>
                     <x-input-label for="update_password_current_password" :value="__('Current Password')" />
-                    <x-text-input id="update_password_current_password" name="current_password" type="password"
-                        class="form-control" autocomplete="current-password" />
+                    <div class="input-group">
+                        <x-text-input id="update_password_current_password" name="current_password" type="password"
+                            class="form-control" autocomplete="current-password" />
+                        <button type="button" class="btn btn-outline-secondary toggle-password">
+                            <i class="fa fa-eye"></i>
+                        </button>
+                    </div>
                     <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
                 </div>
+
                 <div>
                     <x-input-label for="update_password_password" :value="__('New Password')" />
-                    <x-text-input id="update_password_password" name="password" type="password" class="form-control"
-                        autocomplete="new-password" />
+                    <div class="input-group">
+                        <x-text-input id="update_password_password" name="password" type="password" class="form-control"
+                            autocomplete="new-password" />
+                        <button type="button" class="btn btn-outline-secondary toggle-password">
+                            <i class="fa fa-eye"></i>
+                        </button>
+                    </div>
                     <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
                 </div>
 
                 <div>
                     <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
-                    <x-text-input id="update_password_password_confirmation" name="password_confirmation"
-                        type="password" class="form-control" autocomplete="new-password" />
+                    <div class="input-group">
+                        <x-text-input id="update_password_password_confirmation" name="password_confirmation"
+                            type="password" class="form-control" autocomplete="new-password" />
+                        <button type="button" class="btn btn-outline-secondary toggle-password">
+                            <i class="fa fa-eye"></i>
+                        </button>
+                    </div>
                     <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
                 </div>
 
                 <div class="flex items-center gap-4">
-                    {{-- <x-primary-button>{{ __('Save') }}</x-primary-button> --}}
-                    <button type="submit" class="btn btn-primary mt-2"><i class="fa-regular fa-floppy-disk"></i>
-                        Save</button>
+                    <button type="submit" class="btn btn-primary mt-2">
+                        <i class="fa-regular fa-floppy-disk"></i> Save
+                    </button>
                 </div>
             </form>
+
         </div>
     </div>
+
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const togglePasswordButtons = document.querySelectorAll('.toggle-password');
+
+            togglePasswordButtons.forEach(button => {
+                button.addEventListener('click', function() {
+                    const input = this.previousElementSibling; // Input field before the button
+                    const icon = this.querySelector('i');
+
+                    if (input.type === 'password') {
+                        input.type = 'text';
+                        icon.classList.remove('fa-eye');
+                        icon.classList.add('fa-eye-slash');
+                    } else {
+                        input.type = 'password';
+                        icon.classList.remove('fa-eye-slash');
+                        icon.classList.add('fa-eye');
+                    }
+                });
+            });
+        });
+    </script>
+
 </section>
