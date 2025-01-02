ecc018ce496b4025b1efca53d5e0f232e0eae08a
alwisyaban <alwisyaban96@gmail.com>
2025-01-02 09:19:32 +0700

    rename title

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="{{ route('dashboard') }}">{{ config('app.name', 'Laravel') }}</a>
+    <a class="navbar-brand ps-3" href="{{ route('dashboard') }}">QA INJEKSI 💉</a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!">
         <i class="fas fa-bars"></i>
